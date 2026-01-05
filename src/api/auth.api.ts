3de564aa637389be9b3655cd51827d369f4bc8e9
Namal Dilmith <namaldilmith2@gmail.com>
2026-01-06 03:38:19 +0530

    Link correct

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const login= async (data: { name?: string | undefined; email?: string | undefined; id?: string | undefined; profile?:string | undefined; })=>{
     try{
-        const response = await axios.post("https://mern-be-sigma.vercel.app/customer/login-customer",data, {
+        const response = await axios.post("https://blog-phost3.vercel.app/customer/login-customer",data, {
         withCredentials: true,
       });
         return response.data;
