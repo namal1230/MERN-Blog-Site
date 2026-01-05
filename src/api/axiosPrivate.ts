1f258d608e8e4116ea8ad69696c57411e3a0a1ef
Namal Dilmith <namaldilmith2@gmail.com>
2026-01-06 04:44:13 +0530

    Link corrects96

--- src/api/axiosPrivate.ts
+++ src/api/axiosPrivate.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosPrivate = axios.create({
-  baseURL: "https://blog-phost3.vercel.app",
+  baseURL: "https://mern-be-sigma.vercel.app",
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
