6b8d4bd0d2e03cef84c94be993cb890d4f32423c
Namal Dilmith <namaldilmith2@gmail.com>
2026-01-10 04:16:44 +0530

    update some parts279

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -44,11 +44,11 @@ const SignIn: React.FC = () => {
 
     if (!loggedInUser) return;
 
-    setUser(loggedInUser);
+    // setUser(loggedInUser);
 
     const result = await login(loggedInUser);
-    const role = result.user.role;
-    const token = result.token;
+    // const role = result.user.role;
+    // const token = result.token;
     dispatch(setAuth({
       token: result.token,
       name: loggedInUser.name,
@@ -58,13 +58,13 @@ const SignIn: React.FC = () => {
       role: result.user.role, 
     }));
 
-    const userFromApi: User = {
-      id: 123,
-      role: role,
-      token: token,
-    };
+    // const userFromApi: User = {
+    //   id: 123,
+    //   role: role,
+    //   token: token,
+    // };
 
-    setUserAuth(userFromApi);
+    // setUserAuth(userFromApi);
     if (result.user.role === "user") {
       navigate("/home-page");
     } else if (result.user.role === "admin") {
